49c193908138ccb17c2cb132ecf3db15972a0820
elvis <cenelvis@gmail.com>
2022-11-08 10:18:37 -0500

    Title Fix

--- components/newsPage/NewsList.js
+++ components/newsPage/NewsList.js
@@ -5,11 +5,13 @@ const NewsList = (props) => {
 
   return (
     <div className={classes.main}>
-      <img src={data.image} className={classes.image}></img>
-      <h2 className={classes.title}>{data.title}</h2>
-      <p className={classes.date}>{data.date}</p>
-      <p className={classes.description}>{data.description}</p>
-      <a href={data.link}></a>
+      <div className={classes.grid}>
+        <img src={data.image} className={classes.image}></img>
+        <h2 className={classes.title}>{data.title}</h2>
+        <p className={classes.date}>{data.date}</p>
+        <p className={classes.description}>{data.description}</p>
+        <a href={data.link}></a>
+      </div>
     </div>
   );
 };
