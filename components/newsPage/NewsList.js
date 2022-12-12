05c82241dd83c841201ac8556fdecefe8afaa6c4
elvis <cenelvis@gmail.com>
2022-12-11 22:42:29 -0500

    some date work

--- components/newsPage/NewsList.js
+++ components/newsPage/NewsList.js
@@ -3,12 +3,18 @@ import classes from "./NewsList.module.css";
 const NewsList = (props) => {
   const data = props.data;
 
+  const date = data.date;
+  const year = new Date(date).getFullYear();
+  const month = new Date(date).getMonth();
+  const day = new Date(date).getDate();
+  const fullDate = [month, day, year].join("-");
+
   return (
     <div className={classes.main}>
       <div className={classes.grid}>
         <img src={data.image} className={classes.image}></img>
         <h2 className={classes.title}>{data.title}</h2>
-        <p className={classes.date}>{data.date}</p>
+        <p className={classes.date}>{fullDate}</p>
         <p className={classes.description}>{data.description}</p>
         <a href={data.link}></a>
       </div>
